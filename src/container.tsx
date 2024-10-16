be281a2979490664a08d81ed0a541fc5a3127049
yuanfang-dev <yuanfang-dev>
2024-10-16 11:39:13 +0800

    chore: improve tree shaking

--- src/container.tsx
+++ src/container.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { ContainerLayout } from '~/components'
+import { ContainerLayout } from '~/components/layout/container'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
