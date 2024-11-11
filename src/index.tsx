9f4c64698677dccc783fb2dc008022572fed8240
Eric <erichu@valyuu.com>
2024-11-11 16:02:02 +0800

    chore: minor change

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import '~/assets/styles/font.css'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import { MainLayout } from '~/components'
@@ -21,7 +21,7 @@ const router = createBrowserRouter([
   },
 ])
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={new QueryClient()}>
       <RouterProvider router={router} />
